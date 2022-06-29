009e7beb41c97e2eaaea27d413618c46e216082e
Sistema Apurador <heberbbf@gmail.com>
2022-06-29 15:17:55 -0300

    commit

---
+++ app/routes.py
@@ -0,0 +1,340 @@
+from flask import render_template, redirect, url_for, flash, request, abort
+from app import app, database, bcrypt
+from app.forms import FormLogin, FormCriarConta, FormEditarPerfil, FormAcoes, FormCompras, FormVendas, FormApuracao, FormBusca
+from app.models import Usuario, Acoes, Compras, Vendas, Apuracao
+from flask_login import login_user, logout_user, current_user, login_required
+import secrets
+import os
+from PIL import Image
+import calendar
+import datetime
+import locale
+from datetime import date, datetime
+from app import database
+
+import requests
+
+
+
+@app.route('/')
+def home():
+
+    return render_template('home.html')
+
+@app.route('/acoes', methods=['GET', 'POST'])
+@login_required
+def acoes():
+    form_acoes = FormAcoes()
+    if form_acoes.validate_on_submit():
+        acoes = Acoes(ticker=form_acoes.ticker.data, nome=form_acoes.nome.data)
+        database.session.add(acoes)
+        database.session.commit()
+        flash('Ação Cadastrada com Sucesso', 'alert-success')
+        return redirect(url_for('acoes_lista'))
+    return render_template('acoes.html', form_acoes=form_acoes)
+
+@app.route('/acoes/lista')
+def acoes_lista():
+    acoes = Acoes.query.order_by(Acoes.id.asc())
+    return render_template('acoeslista.html', acoes=acoes)
+
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
+
+@app.route('/compras', methods=['GET', 'POST'])
+@login_required
+def compras():
+    form_compras = FormCompras()
+    if form_compras.validate_on_submit():
+
+        #date = datetime.datetime.utcnow()
+        #utc_time = calendar.timegm(date.utctimetuple())
+
+        compras = Compras(id_acao=(form_compras.id_acao.data).id, autor=current_user, data=form_compras.data.data, quantidade=form_compras.quantidade.data, valor_unitario=form_compras.valor_unitario.data, total_taxas=form_compras.total_taxas.data, custo=(form_compras.valor_unitario.data * form_compras.quantidade.data)+form_compras.total_taxas.data)
+        database.session.add(compras)
+        database.session.commit()
+        flash('Compra Registrada com Sucesso', 'alert-success')
+        return redirect(url_for('compras_lista'))
+    return render_template('compras.html', form_compras=form_compras)
+
+@app.route('/compras/lista')
+def compras_lista():
+    compras = Compras.query.filter(Compras.id_usuario==current_user.id).order_by(Compras.id.asc())
+
+    for compra in compras:
+        #compra.data = datetime.datetime.fromtimestamp(compra.data).data.('%d/%m/%Y')
+        compra.data = date.fromisoformat(compra.data)
+        compra.data_formatada = compra.data.strftime('%d/%m/%Y')
+
+        if (compra.valor_unitario is not None):
+            compra.valor = "R${:,.2f}".format(compra.valor_unitario)
+        else:
+            compra.valor = ''
+
+        if (compra.total_taxas is not None):
+            compra.taxas = "R${:,.2f}".format(compra.total_taxas)
+        else:
+            compra.taxas = ''
+
+        if (compra.custo is not None):
+            compra.custo_formatado = "R${:,.2f}".format(compra.custo)
+        else:
+            compra.custo_formatado = ''
+
+    return render_template('compraslista.html', compras=compras)
+
+@app.route('/vendas', methods=['GET', 'POST'])
+@login_required
+def vendas():
+    form_vendas = FormVendas()
+    if form_vendas.validate_on_submit():
+        vendas = Vendas(id_acao=(form_vendas.id_acao.data).id, autor=current_user, data=form_vendas.data.data, quantidade=form_vendas.quantidade.data, valor_unitario=form_vendas.valor_unitario.data, total_taxas=form_vendas.total_taxas.data, total_venda=(form_vendas.valor_unitario.data * form_vendas.quantidade.data)-form_vendas.total_taxas.data, resultado='0.0')
+        database.session.add(vendas)
+        database.session.commit()
+        flash('Compra Registrada com Sucesso', 'alert-success')
+        return redirect(url_for('vendas_lista'))
+    return render_template('vendas.html', form_vendas=form_vendas)
+
+@app.route('/vendas/lista')
+def vendas_lista():
+    vendas = Vendas.query.filter(Vendas.id_usuario==current_user.id).order_by(Vendas.id.asc())
+
+    for venda in vendas:
+
+        #venda.data = datetime.datetime.fromtimestamp(venda.data).strftime('%d/%m/%Y')
+        venda.data = date.fromisoformat(venda.data)
+        venda.data_formatada = venda.data.strftime('%d/%m/%Y')
+
+
+        if (venda.valor_unitario is not None):
+            venda.valor = "R${:,.2f}".format(venda.valor_unitario)
+        else:
+            venda.valor = ''
+
+        if (venda.total_taxas is not None):
+            venda.taxas = "R${:,.2f}".format(venda.total_taxas)
+        else:
+            venda.taxas = ''
+
+        if (venda.total_venda is not None):
+            venda.total_formatado = "R${:,.2f}".format(venda.total_venda)
+        else:
+            venda.total_formatado = ''
+
+
+    return render_template('vendaslista.html', vendas=vendas)
+
+
+@app.route('/apuracao', methods=['GET', 'POST'])
+@login_required
+def apuracao():
+    form_apuracao = FormApuracao()
+    if form_apuracao.validate_on_submit():
+        dataSplit = form_apuracao.data.data.split('/')
+        dataFiltro = dataSplit[1] + '-' + dataSplit[0] 
+        list = []
+        totalResultado = 0
+        totalGeralVendas = 0
+        imposto = 0
+
+        lista =  database.session.execute(
+            """         
+                select ticker, 
+                    sum(qtde_compra), 
+                    sum(qtde_venda),
+                    sum(custo), 
+                    sum(total_venda),
+                    ((sum(total_venda) / sum(qtde_venda)) - (sum(custo) / sum(qtde_compra))) * (sum(qtde_venda))
+                    
+                from (
+                    select a.ticker,
+                            c.quantidade qtde_compra,
+                            0 qtde_venda,
+                            c.custo,
+                            0 total_venda
+                    from compras c,
+                            acoes a
+                    where c.id_acao = a.id
+	                and SUBSTR(c.data, 1, 7) = :data
+	                and c.id_usuario = :user
+
+                    UNION
+
+                    select a.ticker,
+                            0 qtde_compra,
+                            v.quantidade qtde_venda,
+                            0 custo,
+                            v.total_venda
+                    from acoes a,
+                                    vendas v
+                    where v.id_acao = a.id
+	                and SUBSTR(v.data, 1, 7) = :data
+	                and v.id_usuario = :user
+                ) AS X group by ticker
+            """,
+            {'data': dataFiltro, 'user': current_user.id},
+        ).fetchall()
+
+        for tupla in lista:
+            item = {
+                'ticker': tupla[0],
+                'qtdeCompras': tupla[1],
+                'qtdeVendas': tupla[2],
+                'custo': "R${:,.2f}".format(tupla[3]),
+                'totalVendas': "R${:,.2f}".format(tupla[4])
+
+
+            }
+
+
+            if tupla[5] is not None:
+                totalResultado = totalResultado + tupla[5]
+                item.update({'resultado': "R${:,.2f}".format(tupla[5])})
+
+            if tupla[4] is not None:
+                totalGeralVendas = totalGeralVendas + tupla[4]
+                item.update({'totalGeralVendas': "R${:,.2f}".format(tupla[4])})
+
+            list.append(item)
+
+            if totalGeralVendas > 20000:
+                if totalResultado > 0:
+                    imposto = (totalResultado) * 15 / 100
+            else:
+                imposto = 0
+
+
+
+        return render_template('apuracaolista.html', list=list, totalResultado=totalResultado, datacompetencia=form_apuracao.data.data, totalGeralVendas=totalGeralVendas, imposto=imposto)
+    return render_template('apuracao.html', form_apuracao=form_apuracao)
+
+# @app.route('/apuracao/lista')
+# def apuracao_lista():
+#     apuracao = Apuracao.query.order_by(Apuracao.id.asc())
+#     return render_template('apuracaolista.html', apuracao=apuracao)
+
+
+#@app.route('/estoque')
+#def estoque():
+    #estoque = Compras.query.order_by(Compras.data.asc())
+
+     #return render_template('apuracaolista.html', apuracao=apuracao)
+
+@app.route('/usuarios')
+@login_required
+def usuarios():
+    lista_usuarios = Usuario.query.all()
+    return render_template('usuarios.html', lista_usuarios=lista_usuarios)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form_login = FormLogin()
+    form_criarconta = FormCriarConta()
+    if form_login.validate_on_submit() and 'botao_submit_login' in request.form:
+        usuario = Usuario.query.filter_by(email=form_login.email.data).first()
+        if usuario and bcrypt.check_password_hash(usuario.senha, form_login.senha.data):
+            login_user(usuario, remember=form_login.lembrar_dados.data)
+            flash(f'Login feito com sucesso no e-mail: {form_login.email.data}', 'alert-success')
+            par_next = request.args.get('next')
+            if par_next:
+                return redirect(par_next)
+            else:
+                return redirect(url_for('home'))
+        else:
+            flash(f'Falha no Login. E-mail ou Senha Incorretos', 'alert-danger')
+    if form_criarconta.validate_on_submit() and 'botao_submit_criarconta' in request.form:
+        senha_cript = bcrypt.generate_password_hash(form_criarconta.senha.data)
+        usuario = Usuario(username=form_criarconta.username.data, email=form_criarconta.email.data, senha=senha_cript)
+        database.session.add(usuario)
+        database.session.commit()
+        flash(f'Conta criada para o e-mail: {form_criarconta.email.data}', 'alert-success')
+        return redirect(url_for('home'))
+    return render_template('login.html', form_login=form_login, form_criarconta=form_criarconta)
+
+
+@app.route('/sair')
+@login_required
+def sair():
+    logout_user()
+    flash(f'Logout Feito com Sucesso', 'alert-success')
+    return redirect(url_for('home'))
+
+
+@app.route('/perfil')
+@login_required
+def perfil():
+    foto_perfil = url_for('static', filename='fotos_perfil/{}'.format(current_user.foto_perfil))
+    return render_template('perfil.html', foto_perfil=foto_perfil)
+
+
+
+def salvar_imagem(imagem):
+    codigo = secrets.token_hex(8)
+    nome, extensao = os.path.splitext(imagem.filename)
+    nome_arquivo = nome + codigo + extensao
+    caminho_completo = os.path.join(app.root_path, 'static/fotos_perfil', nome_arquivo)
+    tamanho = (400, 400)
+    imagem_reduzida = Image.open(imagem)
+    imagem_reduzida.thumbnail(tamanho)
+    imagem_reduzida.save(caminho_completo)
+    return nome_arquivo
+
+
+@app.route('/perfil/editar', methods=['GET', 'POST'])
+@login_required
+def editar_perfil():
+    form = FormEditarPerfil()
+    if form.validate_on_submit():
+        current_user.email = form.email.data
+        current_user.username = form.username.data
+        if form.foto_perfil.data:
+            nome_imagem = salvar_imagem(form.foto_perfil.data)
+            current_user.foto_perfil = nome_imagem
+
+        database.session.commit()
+        flash('Perfil atualizado com Sucesso', 'alert-success')
+        return redirect(url_for('perfil'))
+    elif request.method == "GET":
+        form.email.data = current_user.email
+        form.username.data = current_user.username
+    foto_perfil = url_for('static', filename='fotos_perfil/{}'.format(current_user.foto_perfil))
+    return render_template('editarperfil.html', foto_perfil=foto_perfil, form=form)
+
+
+
+#@app.route('/cotacao')
+#def cotacao():
+#    url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=PETR3.SA&apikey=KSXJGBLP8S2N3H13'
+#    r = requests.get(url)
+#    cot_acao = r.json()
+
+    #print(cot_acao)
+
+    #return render_template('cotacao.html', cot_acao=cot_acao)
+
+
+@app.route('/busca', methods=['GET', 'POST'])
+@login_required
+def busca():
+    form_busca = FormBusca()
+
+
+    if form_busca.validate_on_submit():
+        busca_acao = form_busca.data.data
+        
+        
+        url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + busca_acao + '.SA&apikey=KSXJGBLP8S2N3H13'
+        r = requests.get(url)
+        cot_acao = r.json()
+        data_cot = (cot_acao['Meta Data']['3. Last Refreshed'])
+
+
+        data_formatada = datetime.strptime(data_cot, "%Y-%m-%d").strftime("%d/%m/%Y")
+
+        return render_template('cotacaolista.html',cot_acao=cot_acao, data_cot=data_cot, form_busca=form_busca, busca_acao=busca_acao, data_formatada=data_formatada)
+
+    return render_template('cotacao.html', form_busca=form_busca)
+
---
+++ app/sistema.db.sql
@@ -0,0 +1,143 @@
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "usuario" (
+	"id"	INTEGER NOT NULL,
+	"username"	VARCHAR NOT NULL,
+	"email"	VARCHAR NOT NULL,
+	"senha"	VARCHAR NOT NULL,
+	"foto_perfil"	VARCHAR,
+	"cursos"	VARCHAR NOT NULL,
+	PRIMARY KEY("id"),
+	UNIQUE("email")
+);
+CREATE TABLE IF NOT EXISTS "post" (
+	"id"	INTEGER NOT NULL,
+	"titulo"	VARCHAR NOT NULL,
+	"corpo"	TEXT NOT NULL,
+	"data_criacao"	DATETIME NOT NULL,
+	"id_usuario"	INTEGER NOT NULL,
+	PRIMARY KEY("id"),
+	FOREIGN KEY("id_usuario") REFERENCES "usuario"("id")
+);
+CREATE TABLE IF NOT EXISTS "taxas" (
+	"id"	INTEGER,
+	"nome"	TEXT NOT NULL,
+	"aliquota"	INTEGER NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+CREATE TABLE IF NOT EXISTS "apuracao" (
+	"id"	INTEGER NOT NULL UNIQUE,
+	"id_usuario"	INTEGER NOT NULL,
+	"data"	TEXT NOT NULL,
+	"data_compra"	TEXT,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+CREATE TABLE IF NOT EXISTS "acoes" (
+	"id"	INTEGER,
+	"nome"	TEXT NOT NULL,
+	"ticker"	TEXT NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+CREATE TABLE IF NOT EXISTS "estoque" (
+	"id"	INTEGER NOT NULL,
+	"id_acao"	INTEGER NOT NULL,
+	"id_usuario"	INTEGER NOT NULL,
+	"saldo_inicial"	INTEGER,
+	"entradas"	INTEGER,
+	"saidas"	INTEGER,
+	"saldo_final"	INTEGER,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+CREATE TABLE IF NOT EXISTS "vendas" (
+	"id"	INTEGER,
+	"id_acao"	INTEGER NOT NULL,
+	"id_usuario"	INTEGER NOT NULL,
+	"data"	TEXT NOT NULL,
+	"quantidade"	INTEGER NOT NULL,
+	"valor_unitario"	REAL NOT NULL,
+	"total_taxas"	REAL NOT NULL,
+	"total_venda"	REAL NOT NULL,
+	"resultado"	REAL NOT NULL DEFAULT 0,
+	PRIMARY KEY("id" AUTOINCREMENT),
+	FOREIGN KEY("id_acao") REFERENCES "acoes"("id"),
+	FOREIGN KEY("id_usuario") REFERENCES "usuario"("id")
+);
+CREATE TABLE IF NOT EXISTS "compras" (
+	"id"	INTEGER,
+	"id_acao"	INTEGER NOT NULL,
+	"id_usuario"	INTEGER NOT NULL,
+	"data"	TEXT NOT NULL,
+	"quantidade"	INTEGER NOT NULL,
+	"valor_unitario"	REAL NOT NULL,
+	"total_taxas"	REAL NOT NULL,
+	"custo"	REAL,
+	PRIMARY KEY("id" AUTOINCREMENT),
+	FOREIGN KEY("id_usuario") REFERENCES "usuario"("id"),
+	FOREIGN KEY("id_acao") REFERENCES "acoes"("id")
+);
+INSERT INTO "usuario" VALUES (1,'heberbbf','heberbbf@gmail.com','$2b$12$pPfffnWn.QrEie2BZ6CrAu9qkGND2aTslCYaQw9lN/uuGlB8J5Iry','images3e7b50599efa847e.png','');
+INSERT INTO "usuario" VALUES (2,'Heber Teste','heber@teste.com','$2b$12$sMFob63w1LXpHkHrGWfK0Ob/QZWswvcZBcdlZUu7nKpMef6y3TXHy','default.jpg','Excel;Power BI;Python');
+INSERT INTO "usuario" VALUES (3,'Murilo','mmissura@gmail.com','$2b$12$afKes5PZBrRae0TDMkTA..JLjN5COe/axWFc6vMwlRUdUov0XLC7W','default.jpg','');
+INSERT INTO "usuario" VALUES (4,'marcelo','marcelotmagalhaes@gmail.com','$2b$12$BNcvA0ZKh.PFsrptun7rSO9idIJR6M3RcV9kcaQZcwOQpOvvUW5Ie','default.jpg','Não Informado');
+INSERT INTO "usuario" VALUES (5,'Luciano','dsibr@hotmail.com','$2b$12$nxyf4kseLWiv1uALlK7Qce6ngvmATPRnwaXzdFYW65//.NPQRfdOC','antiga_e0493252fb889e30.jpg','Não Informado');
+INSERT INTO "post" VALUES (1,'post teste','fkdjfklakladnbk','2021-11-10 22:52:59.285666',3);
+INSERT INTO "post" VALUES (2,'Teste BD','Teste','2021-11-17 13:45:43.210605',2);
+INSERT INTO "post" VALUES (3,'Testando o sistema','Post realizado para testes','2021-11-17 19:54:35.603027',1);
+INSERT INTO "post" VALUES (4,'Teste Usuário','Teste ','2021-11-17 19:57:08.418768',1);
+INSERT INTO "post" VALUES (5,'Receita','Ingredientes
+Massa do Bolo de Cenoura
+3 cenouras médias (250g)
+4 ovos
+meia xícara (chá) de óleo
+2 e meia xícaras (chá) de farinha de trigo
+2 xícaras (chá) de açúcar
+1 colher (sopa) de fermento em pó
+Cobertura de Brigadeiro
+1 Leite MOÇA® (lata ou caixinha) 395g
+1 colher (sopa) de manteiga
+meia xícara (chá) de Chocolate em Pó NESTLÉ® DOIS FRADES®
+meia xícara (chá) de chocolate granulado
+Modo de preparo
+Massa do Bolo de Cenoura
+Em um liquidificador, bata as cenouras, os ovos e o óleo.
+
+Despeje a mistura em um recipiente e misture o açúcar e a farinha de trigo peneirada com o fermento.
+
+Coloque em uma fôrma retangular (20 x 30 cm) untada, e leve ao forno médio (180°C), preaquecido, por 40 minutos.
+
+Enquanto isso, prepare a cobertura de brigadeiro.
+
+Cobertura de Brigadeiro
+Em uma panela coloque o Leite MOÇA, o Chocolate em Pó DOIS FRADES e a manteiga e leve ao fogo baixo, mexendo sempre, até começar a desprender da panela.
+
+Despeje ainda quente sobre o bolo, distribua o chocolate granulado e deixe esfriar.','2021-11-17 19:59:08.536638',1);
+INSERT INTO "acoes" VALUES (1,' Petroleo Brasileiro SA - Petrobras','PETR3');
+INSERT INTO "acoes" VALUES (2,'Magazine Luiza','MGLU3');
+INSERT INTO "acoes" VALUES (3,'VALE S.A.','VALE3');
+INSERT INTO "acoes" VALUES (4,'Itaú Unibanco S.A.','ITUB4');
+INSERT INTO "acoes" VALUES (5,'Bradesco S.A.','BBDC4');
+INSERT INTO "acoes" VALUES (6,'Banco do Brasil S.A.','BBAS3');
+INSERT INTO "acoes" VALUES (7,'Via Varejo','VIIA3');
+INSERT INTO "acoes" VALUES (8,'Embraer S.A.','EMBR3');
+INSERT INTO "vendas" VALUES (15,4,2,'2021-01-10',100,25.0,5.0,2495.0,0.0);
+INSERT INTO "vendas" VALUES (16,7,2,'2021-01-15',40,28.0,2.0,1118.0,0.0);
+INSERT INTO "vendas" VALUES (17,8,2,'2021-02-22',100,25.5,5.0,2545.0,0.0);
+INSERT INTO "vendas" VALUES (18,6,2,'2021-02-25',100,29.0,5.0,2895.0,0.0);
+INSERT INTO "vendas" VALUES (19,3,2,'2021-03-15',1000,29.5,20.0,29480.0,0.0);
+INSERT INTO "vendas" VALUES (20,2,2,'2021-03-12',600,28.5,12.0,17088.0,0.0);
+INSERT INTO "vendas" VALUES (21,1,2,'2021-04-10',300,30.58,10.0,9164.0,0.0);
+INSERT INTO "vendas" VALUES (22,7,2,'2021-04-11',150,24.8,10.0,3710.0,0.0);
+INSERT INTO "vendas" VALUES (23,6,2,'2021-04-05',350,45.0,10.0,15740.0,0.0);
+INSERT INTO "vendas" VALUES (24,4,1,'2021-11-10',10,8.0,90.0,-10.0,0.0);
+INSERT INTO "compras" VALUES (20,4,2,'2021-01-01',100,20.0,5.0,2005.0);
+INSERT INTO "compras" VALUES (21,7,2,'2021-01-05',80,30.0,2.0,2402.0);
+INSERT INTO "compras" VALUES (22,8,2,'2021-02-01',100,25.0,5.0,2505.0);
+INSERT INTO "compras" VALUES (23,6,2,'2021-02-20',200,30.0,10.0,6010.0);
+INSERT INTO "compras" VALUES (24,3,2,'2021-03-01',1000,28.0,25.0,28025.0);
+INSERT INTO "compras" VALUES (25,2,2,'2021-03-02',800,30.0,15.0,24015.0);
+INSERT INTO "compras" VALUES (26,1,2,'2021-04-01',300,30.0,10.0,9010.0);
+INSERT INTO "compras" VALUES (27,7,2,'2021-04-02',300,22.0,15.0,6615.0);
+INSERT INTO "compras" VALUES (28,6,2,'2021-04-01',400,40.0,40.0,16040.0);
+INSERT INTO "compras" VALUES (29,4,2,'2021-05-01',100,100.0,10.0,10010.0);
+INSERT INTO "compras" VALUES (30,1,1,'2021-01-01',5,80.0,10.0,410.0);
+INSERT INTO "compras" VALUES (31,1,5,'2022-05-10',100,100.0,2.0,10002.0);
+COMMIT;
